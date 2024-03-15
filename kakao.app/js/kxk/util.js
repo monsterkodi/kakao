111e9cf54dac5e4d4806f5b190e2a5a8d9816fcc
monsterkodi <monsterkodi@gmx.net>
2024-03-15 21:29:47 +0100

    bright

--- kakao.app/js/kxk/util.js
+++ kakao.app/js/kxk/util.js
@@ -13,7 +13,7 @@ isEqual = function (a, b)
         {
             return false
         }
-        for (var _14_21_ = index = 0, _14_25_ = a.length; (_14_21_ <= _14_25_ ? index < a.length : index > a.length); (_14_21_ <= _14_25_ ? ++index : --index))
+        for (var _13_21_ = index = 0, _13_25_ = a.length; (_13_21_ <= _13_25_ ? index < a.length : index > a.length); (_13_21_ <= _13_25_ ? ++index : --index))
         {
             if (!isEqual(a[index],b[index]))
             {
@@ -33,7 +33,7 @@ export default {isEqual:isEqual,zip:function (...args)
     {
         return a.length
     }))
-    for (var _33_17_ = i = 0, _33_21_ = maxLen; (_33_17_ <= _33_21_ ? i < maxLen : i > maxLen); (_33_17_ <= _33_21_ ? ++i : --i))
+    for (var _32_17_ = i = 0, _32_21_ = maxLen; (_32_17_ <= _32_21_ ? i < maxLen : i > maxLen); (_32_17_ <= _32_21_ ? ++i : --i))
     {
         result.push(args.map(function (a)
         {
@@ -67,7 +67,7 @@ export default {isEqual:isEqual,zip:function (...args)
 {
     var index
 
-    for (var _52_21_ = index = arr.length - 1, _52_35_ = 0; (_52_21_ <= _52_35_ ? index <= 0 : index >= 0); (_52_21_ <= _52_35_ ? ++index : --index))
+    for (var _51_21_ = index = arr.length - 1, _51_35_ = 0; (_51_21_ <= _51_35_ ? index <= 0 : index >= 0); (_51_21_ <= _51_35_ ? ++index : --index))
     {
         if (pred(arr[index]))
         {
@@ -82,10 +82,10 @@ export default {isEqual:isEqual,zip:function (...args)
     if (!_k_.empty(arr) && _k_.isArr(arr))
     {
         var list = _k_.list(items)
-        for (var _61_21_ = 0; _61_21_ < list.length; _61_21_++)
+        for (var _60_21_ = 0; _60_21_ < list.length; _60_21_++)
         {
-            item = list[_61_21_]
-            for (var _62_29_ = index = arr.length - 1, _62_43_ = 0; (_62_29_ <= _62_43_ ? index <= 0 : index >= 0); (_62_29_ <= _62_43_ ? ++index : --index))
+            item = list[_60_21_]
+            for (var _61_29_ = index = arr.length - 1, _61_43_ = 0; (_61_29_ <= _61_43_ ? index <= 0 : index >= 0); (_61_29_ <= _61_43_ ? ++index : --index))
             {
                 if (cmp(arr[index],item))
                 {
@@ -101,9 +101,9 @@ export default {isEqual:isEqual,zip:function (...args)
 
     result = []
     var list = _k_.list(arr)
-    for (var _70_17_ = 0; _70_17_ < list.length; _70_17_++)
+    for (var _69_17_ = 0; _69_17_ < list.length; _69_17_++)
     {
-        item = list[_70_17_]
+        item = list[_69_17_]
         if (!(_k_.in(item,result)))
         {
             result.push(item)
@@ -116,14 +116,14 @@ export default {isEqual:isEqual,zip:function (...args)
 
     result = []
     var list = _k_.list(arr)
-    for (var _78_17_ = 0; _78_17_ < list.length; _78_17_++)
+    for (var _77_17_ = 0; _77_17_ < list.length; _77_17_++)
     {
-        item = list[_78_17_]
+        item = list[_77_17_]
         add = true
         var list1 = _k_.list(result)
-        for (var _80_22_ = 0; _80_22_ < list1.length; _80_22_++)
+        for (var _79_22_ = 0; _79_22_ < list1.length; _79_22_++)
         {
-            ritem = list1[_80_22_]
+            ritem = list1[_79_22_]
             if (isEqual(item,ritem))
             {
                 add = false
@@ -150,14 +150,14 @@ export default {isEqual:isEqual,zip:function (...args)
     }
     result = []
     var list = _k_.list(arr)
-    for (var _94_17_ = 0; _94_17_ < list.length; _94_17_++)
+    for (var _93_17_ = 0; _93_17_ < list.length; _93_17_++)
     {
-        item = list[_94_17_]
+        item = list[_93_17_]
         add = true
         var list1 = _k_.list(result)
-        for (var _96_22_ = 0; _96_22_ < list1.length; _96_22_++)
+        for (var _95_22_ = 0; _95_22_ < list1.length; _95_22_++)
         {
-            ritem = list1[_96_22_]
+            ritem = list1[_95_22_]
             if (prop(item) === prop(ritem))
             {
                 add = false
@@ -199,12 +199,12 @@ export default {isEqual:isEqual,zip:function (...args)
     })
 },defaults:function (obj, def)
 {
-    var key, val, _126_21_
+    var key, val, _125_21_
 
     for (key in def)
     {
         val = def[key]
-        obj[key] = ((_126_21_=obj[key]) != null ? _126_21_ : val)
+        obj[key] = ((_125_21_=obj[key]) != null ? _125_21_ : val)
     }
     return obj
 },pickBy:function (obj, pred)
