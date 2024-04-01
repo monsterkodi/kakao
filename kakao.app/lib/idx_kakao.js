17fbe954ba5f30c535534432164cc84fb82b4025
monsterkodi <monsterkodi@gmx.net>
2024-04-01 17:16:34 +0200

    del lib_kakao

--- kakao.app/lib/idx_kakao.js
+++
@@ -1,9 +0,0 @@
-
-import _stylus from 'stylus'
-import _pug    from 'pug'
-
-let stylus = (s) => { return _stylus.render(s) }
-let pug    = (s) => { return _pug.render(s, { pretty: true }) } 
-
-export default { stylus, pug }
-
