ae33f290614cf3747169b9e04ac411fcb4d181df
monsterkodi <monsterkodi@gmx.net>
2024-04-04 00:37:58 +0200

    styl

--- kakao.app/js/kxk/about.js
+++ kakao.app/js/kxk/about.js
@@ -9,7 +9,7 @@ import kakao from "../kakao.js"
 
 kakao.init(function ()
 {
-    var main, _26_22_
+    var main
 
     kakao('window.setSize',250,250)
     kakao('window.center')
@@ -23,6 +23,5 @@ kakao.init(function ()
     }
     main = $('#main')
     main.classList.add('app-drag-region')
-    window.aboutImage = ((_26_22_=window.aboutImage) != null ? _26_22_ : kakao.bundle.img('about.png'))
     return elem('div',{class:'about',id:'about',parent:main,children:[elem('img',{class:'image',src:window.aboutImage}),elem('div',{class:'version',id:'version',text:'0.0.0'})]})
 })
