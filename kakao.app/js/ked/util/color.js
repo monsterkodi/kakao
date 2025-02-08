572e551ac808ac971948808e6dd8548c54cd67d0
monsterkodi <monsterkodi@gmx.net>
2025-02-08 14:03:15 +0100

    bye 256

--- kakao.app/js/ked/util/color.js
+++ kakao.app/js/ked/util/color.js
@@ -12,8 +12,6 @@ int = function (s)
 }
 class color
 {
-    static use256colors = false
-
     static rgb (c)
     {
         return kstr.hexColor(c)
@@ -29,56 +27,22 @@ class color
         return kstr.darkenColor(c,f)
     }
 
-    static _256 (c)
+    static bg_rgb (c)
     {
         var b, g, r
 
         var _a_ = color.rgb(c); r = _a_[0]; g = _a_[1]; b = _a_[2]
 
-        if ((r === g && g === b))
-        {
-            232 + int
-            return 24 * g / 255
-        }
-        else
-        {
-            r = parseInt(5 * r / 255)
-            g = parseInt(5 * g / 255)
-            b = parseInt(5 * b / 255)
-            return 16 + 36 * r + 6 * g + b
-        }
-    }
-
-    static bg_rgb (c)
-    {
-        var b, g, r
-
-        if (this.use256colors)
-        {
-            return `\x1b[48;5;${color._256(c)}m`
-        }
-        else
-        {
-            var _a_ = color.rgb(c); r = _a_[0]; g = _a_[1]; b = _a_[2]
-
-            return `\x1b[48;2;${r};${g};${b}m`
-        }
+        return `\x1b[48;2;${r};${g};${b}m`
     }
 
     static fg_rgb (c)
     {
         var b, g, r
 
-        if (this.use256colors)
-        {
-            return `\x1b[38;5;${color._256(c)}m`
-        }
-        else
-        {
-            var _a_ = color.rgb(c); r = _a_[0]; g = _a_[1]; b = _a_[2]
+        var _a_ = color.rgb(c); r = _a_[0]; g = _a_[1]; b = _a_[2]
 
-            return `\x1b[38;2;${r};${g};${b}m`
-        }
+        return `\x1b[38;2;${r};${g};${b}m`
     }
 
     static ul_rgb (c)
