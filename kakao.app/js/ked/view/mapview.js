bb250e75bcf514ddf59229c01c4cfce3bdcc90a0
monsterkodi <monsterkodi@gmx.net>
2025-02-17 03:55:22 +0100

    misc

--- kakao.app/js/ked/view/mapview.js
+++ kakao.app/js/ked/view/mapview.js
@@ -1,4 +1,4 @@
-var _k_ = {empty: function (l) {return l==='' || l===null || l===undefined || l!==l || typeof(l) === 'object' && Object.keys(l).length === 0}, list: function (l) {return l != null ? typeof l.length === 'number' ? l : [] : []}, in: function (a,l) {return (typeof l === 'string' && typeof a === 'string' && a.length ? '' : []).indexOf.call(l,a) >= 0}, clamp: function (l,h,v) { var ll = Math.min(l,h), hh = Math.max(l,h); if (!_k_.isNum(v)) { v = ll }; if (v < ll) { v = ll }; if (v > hh) { v = hh }; if (!_k_.isNum(v)) { v = ll }; return v }, isNum: function (o) {return !isNaN(o) && !isNaN(parseFloat(o)) && (isFinite(o) || o === Infinity || o === -Infinity)}}
+var _k_ = {list: function (l) {return l != null ? typeof l.length === 'number' ? l : [] : []}, empty: function (l) {return l==='' || l===null || l===undefined || l!==l || typeof(l) === 'object' && Object.keys(l).length === 0}, in: function (a,l) {return (typeof l === 'string' && typeof a === 'string' && a.length ? '' : []).indexOf.call(l,a) >= 0}, clamp: function (l,h,v) { var ll = Math.min(l,h), hh = Math.max(l,h); if (!_k_.isNum(v)) { v = ll }; if (v < ll) { v = ll }; if (v > hh) { v = hh }; if (!_k_.isNum(v)) { v = ll }; return v }, isNum: function (o) {return !isNaN(o) && !isNaN(parseFloat(o)) && (isFinite(o) || o === Infinity || o === -Infinity)}}
 
 var floor, mapview, pow
 
@@ -76,10 +76,6 @@ mapview = (function ()
     {
         var id
 
-        if (!_k_.empty(this.images))
-        {
-            lf('clear',this.images.length)
-        }
         var list = _k_.list(this.images)
         for (var _a_ = 0; _a_ < list.length; _a_++)
         {
@@ -205,7 +201,6 @@ mapview = (function ()
         {
             return
         }
-        lf('map draw',this.images.length)
         var list = _k_.list(this.images)
         for (y = 0; y < list.length; y++)
         {
