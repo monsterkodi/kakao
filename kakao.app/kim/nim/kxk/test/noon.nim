40c6e8a7e82b3524d71f5513bbd8041603d55dc7
monsterkodi <monsterkodi@gmx.net>
2025-05-25 01:09:36 +0200

    nonoon

--- kakao.app/kim/nim/kxk/test/noon.nim
+++
@@ -1,8 +0,0 @@
-import ../noon
-
-template t(a:string, b:JsonNode) = testCmp(a, parseNoon(a), b, instantiationInfo())
-suite "noon": 
-    test "basic": 
-        t("", %(""))
-        # t "1"  %(1)
-        # t "hello  world"  %({"hello": "world"}.toTable)
